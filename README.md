114a8e6eb8e3fb101939d416d4704096b274e619
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-21 03:44:58 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# arm-neon-example
+
+## Install Poac
+
+```console
+$ brew install poac
+```
+
+## Run this example
+
+```console
+$ git clone https://github.com/ken-matsui/arm-neon-example.git
+$ cd arm-neon-example
+$ poac run
+   Compiling 1/1: arm-neon-example v0.1.0 ("/tmp/arm-neon-example")
+    Finished debug target(s) in 0.42s
+     Running "/tmp/arm-neon-example/poac-out/debug/arm-neon-example"
+uint8x16_test
+b[0] = 0
+b[1] = 1
+b[2] = 2
+b[3] = 3
+b[4] = 4
+b[5] = 5
+b[6] = 6
+b[7] = 7
+b[8] = 8
+b[9] = 9
+b[10] = 10
+b[11] = 11
+b[12] = 12
+b[13] = 13
+b[14] = 14
+b[15] = 15
+
+uint8x16x2_test
+b[0] = 0
+b[1] = 1
+b[2] = 2
+b[3] = 3
+b[4] = 4
+b[5] = 5
+b[6] = 6
+b[7] = 7
+b[8] = 8
+b[9] = 9
+b[10] = 10
+b[11] = 11
+b[12] = 12
+b[13] = 13
+b[14] = 14
+b[15] = 15
+b[16] = 16
+b[17] = 17
+b[18] = 18
+b[19] = 19
+b[20] = 20
+b[21] = 21
+b[22] = 22
+b[23] = 23
+b[24] = 24
+b[25] = 25
+b[26] = 26
+b[27] = 27
+b[28] = 28
+b[29] = 29
+b[30] = 30
+b[31] = 31
+
+uint32x4_test
+b[0] = 0
+b[1] = 1
+b[2] = 2
+b[3] = 3
+
+uint32x4x2_test
+b[0] = 0
+b[1] = 1
+b[2] = 2
+b[3] = 3
+b[4] = 4
+b[5] = 5
+b[6] = 6
+b[7] = 7
+```
---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+#include <cassert>
+#include <cstdint>
+#include <arm_neon.h>
+#include <iostream>
+
+template <typename T>
+void init(T& a) {
+  for (int i = 0; i < sizeof(a) / sizeof(a[0]); ++i) {
+    a[i] = i;
+  }
+}
+
+template <typename T>
+void print_and_assert(const T& b) {
+  for (int i = 0; i < sizeof(b) / sizeof(b[0]); ++i) {
+    assert(b[i] == i);
+    std::cout << "b[" << i << "] = " << (int)b[i] << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+// (Uint8, 16)
+void uint8x16_test() {
+  std::cout << __func__ << std::endl;
+  constexpr std::size_t kN = 16;
+
+  uint8_t a[kN] = {};
+  init(a);
+
+  uint8x16_t va = vld1q_u8(a);
+
+  uint8_t b[kN] = {};
+  vst1q_u8(b, va);
+
+  print_and_assert(b);
+}
+
+// (Uint8, 32)
+void uint8x16x2_test() {
+  std::cout << __func__ << std::endl;
+  constexpr std::size_t kN = 32;
+
+  uint8_t a[kN] = {};
+  init(a);
+
+  uint8x16x2_t va = vld2q_u8(a);
+
+  uint8_t b[kN] = {};
+  vst2q_u8(b, va);
+
+  print_and_assert(b);
+}
+
+// (Uint32, 4)
+void uint32x4_test() {
+  std::cout << __func__ << std::endl;
+  constexpr std::size_t kN = 4;
+
+  uint32_t a[kN] = {};
+  init(a);
+
+  uint32x4_t va = vld1q_u32(a);
+
+  uint32_t b[kN] = {};
+  vst1q_u32(b, va);
+
+  print_and_assert(b);
+}
+
+// (Uint32, 8)
+void uint32x4x2_test() {
+  std::cout << __func__ << std::endl;
+  constexpr std::size_t kN = 8;
+
+  uint32_t a[kN] = {};
+  init(a);
+
+  uint32x4x2_t va = vld2q_u32(a);
+
+  uint32_t b[kN] = {};
+  vst2q_u32(b, va);
+
+  print_and_assert(b);
+}
+
+int main(int argc, char** argv) {
+  uint8x16_test();
+  uint8x16x2_test();
+  uint32x4_test();
+  uint32x4x2_test();
+}
